9b6b76440e456bb6f1ad9a88e49937233a67f24a
Remco Bras <vrsoft@gmail.com>
2008-02-10 16:40:49 +0100

    Added prototype sed-based convertor macros.
    Basically, the idea is to have sed take care of the stuff the C preprocessor won't do: LISP-macro-like constructs in C.
    Currently, this requires GNU sed because of my use of \U and \E (Both of which could be avoided if the typeinfo enum macros were renamed to TYPE_<type-as-typedeffed> instead of the all-uppercase versions. That however would be extremely unesthetic and trading lots of ugliness for a bit of ugliness we can avoid is relatively lame.
    
    This still needs to be integrated into the build system, probably using some kind of customized suffix rule.

---
+++ convertors.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#Copyright 2008 Remco Bras
+#Copyright Remco Bras 2007
+#This file is part of RPGE.
+#
+#RPGE is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 3 of the License, or
+#(at your option) any later version.
+#
+#RPGE is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+#
+#convertors.sh: Implement the convertors(type) and convertor_headers(type) sed-based macros.
+#
+
+sed 's/convertors(\(.*\));/inline object\
+                         make_\1_obj(\1 foo)\
+                         {\
+                           object o;\
+                           o.data=malloc(sizeof(\1));\
+                           o.typeinfo = TYPE_\U\1\E;\
+                           *((\1*)o.data)=foo;\
+                           return o;\
+                         }\
+                         inline \1\
+                         get_obj_\1 (object o)\
+                         {\
+                           return *((\1*)o.data);\
+                         }/g' $1 -i
+                         
+sed 's/convertor_headers(\(.*\));/inline object make_\1_obj(\1 foo);\
+                                  inline \1 get_obj_\1 (object o);/g' $1 -i
+indent $1
