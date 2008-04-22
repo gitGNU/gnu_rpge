26a10a685f1782923f29a0c2f27b32f4fc1a947b
Remco Bras <vrsoft@gmail.com>
2008-04-22 19:58:35 +0200

    Makefile.am: Remove redundancy (let conf handle itself); scm/Makefile.am: add dialog.scm; scm/dialog.scm: Implement prototype dialog API.
    This API still needs quite a bit of work and some glue to add it into the whole key handling system. It does seem to work, but dialogs-next throws an error if the list of
    strings to be displayed is null.

---
+++ scm/dialog.scm
@@ -0,0 +1,80 @@
+;Copyright  2008 Remco Bras
+;
+;This file is part of the RPGE.
+;
+;RPGE is free software; you can redistribute it and/or modify
+;it under the terms of the GNU General Public License as published by
+;the Free Software Foundation; either version 3 of the License, or
+;(at your option) any later version.
+;
+;RPGE is distributed in the hope that it will be useful,
+;but WITHOUT ANY WARRANTY; without even the implied warranty of
+;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;GNU General Public License for more details.
+;
+;You should have received a copy of the GNU General Public License
+;    along with this program.  If not, see <http://www.gnu.org/licenses/>
+;
+
+(define dialog-config (make-table-closure))
+(define dialogs (make-table-closure))
+
+(define (dialogs-init initial-width initial-height initial-font sprite-name sprite-width sprite-height)
+  (add-to-table! (dialog-config) 'dimensions (cons initial-width initial-height))
+  (add-to-table! (dialog-config) 'font initial-font)
+  (add-to-table! (dialog-config) 'sprite sprite-name)
+  (add-to-table! (dialog-config) 'sprite-width sprite-width)
+  (add-to-table! (dialog-config) 'sprite-height sprite-height))
+
+(define (get-dialog id)
+  (get-from-table (dialogs) id))
+
+(define (make-dialog dialogid x y stringlist)
+  (let ((sizes (get-from-table (dialog-config) 'dimensions)) (font (get-from-table (dialog-config) 'font)))
+    (let ((w (create-window (car sizes) (cdr sizes) x y (get-from-table (dialog-config) 'sprite) (get-from-table (dialog-config) 'sprite-width) (get-from-table (dialog-config) 'sprite-width))))
+      (let ((dialog (list w -1 font stringlist)))
+	(add-to-table! (dialogs) dialogid dialog)
+	(dialogs-next dialogid)
+	dialog))))
+
+(define (dialogs-next dialogid)
+  (let ((dialog (get-dialog dialogid)))
+  (cond
+   ((null? dialog) '())
+   (else
+    (if (> (get-dialog-text dialog) -1)
+	(destroy-text (get-dialog-text dialog)))
+    (let ((text (get-next-text-string dialog))) 
+      (if (null? text) (destroy-dialog! dialogid) (set-dialog-text! dialog (make-text (get-dialog-text-x dialog) (get-dialog-text-y dialog) text (get-dialog-font dialog) 255 255 255))))))))
+
+(define get-dialog-text cadr)
+
+(define get-dialog-window car)
+
+(define get-dialog-font caddr)
+
+(define (get-next-text-string dialog)
+  (let ((list (cadddr dialog)))
+    (if (null? list) '() 
+	(let ((t (car list)))
+	  (set-car! (cdddr dialog) (cdr list))
+	  t))))
+
+(define (get-dialog-text-x dialog)
+  (let ((w (get-dialog-window dialog)))
+    (let ((sizes (get-window-dimensions w))(position (get-window-coordinates w)))
+      (+ (/ (car sizes) 10) (car position)))))
+
+(define (get-dialog-text-y dialog)
+  (let ((w (get-dialog-window dialog)))
+    (let ((sizes (get-window-dimensions w)) (position (get-window-coordinates w)))
+      (+ (/ (cdr sizes) 10) (cdr position)))))
+
+(define (set-dialog-text! dialog val)
+  (set-car! (cdr dialog) val))
+
+(define (destroy-dialog! id)
+  (let ((dialog (get-dialog id)))
+    (remove-window (get-dialog-window dialog))
+    (destroy-text (get-dialog-text dialog))
+    (remove-from-table! (dialogs) id)))
