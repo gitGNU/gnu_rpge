5e3d8521568dc042bee43a7c3ec94b4c13a8308f
Remco Bras <zero@zero-laptop.(none)>
2008-10-15 23:01:22 +0200

    Collect added, used in border window creation.

--- scm/border-window.scm
+++ scm/border-window.scm
@@ -15,6 +15,8 @@
 ;You should have received a copy of the GNU General Public License
 ;    along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+(use-modules (srfi srfi-1))
+
 (define (make-bordered-window width height x y spritename cliprect)
   (let* ((startx (car cliprect))
 	 (starty (cadr cliprect))
@@ -23,24 +25,31 @@
 	 (mid-width (- width (* border-width 2)))
 	 (mid-height (- height (* border-height 2))))
     ;Needs optimization and generalization
-    (do ((i 0 (1+ i))
-	 (acc '()))
-	((= i 3) (reverse! acc))
-      (do ((j 0 (1+ j)))
-	  ((= j 3) '())
-	  (set! acc
-		(cons (create-window (if (= i 1) mid-width border-width)
-				     (if (= j 1) mid-height border-height)
-				     (+ x (if (>= i 1) border-width 0)
-					(if (= i 2) mid-width 0))
-				     (+ y (if (>= j 1) border-height 0)
-					(if (= j 2) mid-height 0))
-				     spritename
-				     (make-rect (+ startx (* i border-width))
-						(+ starty (* j border-height))
-						border-width
-						border-height))
-		      acc))))))
+    (collect-append 
+     ((i 0 (1+ i)))
+     (= i 3)
+     (collect ((j 0 (1+ j)))
+	      (= j 3)
+	      (create-window (if (= i 1) mid-width border-width)
+			     (if (= j 1) mid-height border-height)
+			     (+ x (if (>= i 1) border-width 0)
+				(if (= i 2) mid-width 0))
+			     (+ y (if (>= j 1) border-height 0)
+				(if (= j 2) mid-height 0))
+			     spritename
+			     (make-rect (+ startx (* i border-width))
+					(+ starty (* j border-height))
+					border-width
+					border-height))))))
 				     
 (define (remove-bordered-window bw)
   (for-each remove-window bw))
+
+(define (dispatched-get-window-dimensions w)
+  (if (pair? w)
+      (cons (/ (fold + 0 (map (lambda (win) (car (get-window-dimensions win))) w)) 3)
+	    (/ (fold + 0 (map (lambda (win) (cdr (get-window-dimensions win))) w)) 3))
+      (get-window-dimensions w)))
+
+(define (dispatched-remove-window w)
+  ((if (pair? w) remove-bordered-window remove-window) w))
